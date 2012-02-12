ac6f1489f5b9a5e5911a5c567b0b55a631b6bf8e
Aaron Mangel <amangel@gmail.com>
2012-02-12 11:08:01 -0600

    moved the initial driver to the default package

---
+++ src/driver.java
@@ -0,0 +1,39 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.zza.scanner.*; 
+
+
+public class driver {
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+        StringBuffer sb = new StringBuffer();
+        String programToUse ="";
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(args[0])); 
+            String str;
+            while ((str = in.readLine()) != null) {
+                sb.append(str+"\n");
+            }
+            in.close();
+            programToUse =  sb.toString();
+        } catch (ArrayIndexOutOfBoundsException e){
+            e.printStackTrace();
+            System.out.println("Scanner requires a file name as input to create a token list.");
+            System.out.println("Example usage: [java driver ../sampleFlairPrograms/factorial.fl]");
+            System.exit(-1);
+        } catch (IOException e) {
+        }
+        
+        CompilerStateScanner s = new CompilerStateScanner(programToUse);
+        
+        
+    }
+    
+}
