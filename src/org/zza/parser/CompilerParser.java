280687bac2a20d8acf2e195f5b469aa6f7ee2298
Zach Abbott <abbottz@uni.edu>
2012-02-22 14:39:02 -0600

    Added table driven parser alg

--- src/org/zza/parser/CompilerParser.java
+++ src/org/zza/parser/CompilerParser.java
@@ -4,9 +4,47 @@ import org.zza.scanner.CompilerTokenStream;
 
 
 public class CompilerParser {
-
+	
+	private Stack ruleStack;
+	private Table ruleTable;
+	
     public CompilerParser(CompilerTokenStream tokenStream) {
         
+    }
+    
+    public run(foo bar) {
+    	ruleStack.push("$");
+    	ruleStack.push("start symbol");
+    	while(ruleStack.peek() != "$")
+    	{
+    		if(ruleStack.peek().terminal?)
+    		{
+    			if(ruleStack.peek() == i)
+    			{
+    				ruleStack.pop();
+    				i.consume();
+    			}
+    			else
+    			{
+    				//FAIL SOME WAY
+    			}
+    		}
+    		else
+    		{
+    			if(ruleTable.find(A,i).containsRule?)
+    			{
+    				ruleStack.pop();
+    				ruleStack.push(ruleTable.find(A,i));
+    			}
+    			else
+    			{
+    				//Fail some way
+    			}
+    		}
+    			
+    		
+    	}
+    	
     }
     
 }
