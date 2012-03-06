ab6673a66e049767ccdc796ae35d5af07c662647
Alexander Preston <zmpinqc@gmail.com>
2012-03-06 11:25:24 -0600

    categorized flair programs into valid and invalid.  Created a two bash scripts to run valid and invalid programs. Will create one soon which will run all programs.  Currently the output of the compiler calls in these scripts goes to standard-out.  Looking at potentially making each of these go to an output file so that they can be viewed individually and have some persistence (which could make for better debugging for us).

---
+++ runValidPrograms.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+echo "Will run all valid Flair Programs found in ../sampleFlairPrograms/validPrograms/"
+cd bin/
+java driver ../sampleFlairPrograms/validPrograms/abs.fl
+java driver ../sampleFlairPrograms/validPrograms/addHundred.fl
+java driver ../sampleFlairPrograms/validPrograms/cosine.fl
+java driver ../sampleFlairPrograms/validPrograms/everything.fl
+java driver ../sampleFlairPrograms/validPrograms/factorial.fl
+java driver ../sampleFlairPrograms/validPrograms/funccall.fl
+java driver ../sampleFlairPrograms/validPrograms/hello-var.fl
+java driver ../sampleFlairPrograms/validPrograms/HelloVar.fl
+java driver ../sampleFlairPrograms/validPrograms/hello-world.fl
+java driver ../sampleFlairPrograms/validPrograms/HelloWorld.fl
+java driver ../sampleFlairPrograms/validPrograms/integerDivison.fl
+java driver ../sampleFlairPrograms/validPrograms/LeadingMinusSigns.fl
+java driver ../sampleFlairPrograms/validPrograms/newtonstest.fl
+java driver ../sampleFlairPrograms/validPrograms/triangle.fl
+java driver ../sampleFlairPrograms/validPrograms/whileiftest.fl
+
